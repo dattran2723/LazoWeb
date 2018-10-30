a53c245d0ff1a10decea9a80b303915d12613cf0
Tran Dat <Tran Dat@DESKTOP-SLFVQSH>
2018-10-30 17:09:10 +0700

    insert customers.cs

---
+++ LazoWeb/Models/Customers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LazoWeb.Models
+{
+    public class Customers
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [DisplayName("Họ và tên")]
+        [Required(ErrorMessage ="Vui lòng nhập họ và tên")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [DisplayName("Tên công ty")]
+        [Required(ErrorMessage = "Vui lòng nhập tên công ty")]
+        public string Company { get; set; }
+
+        [DisplayName("Số lượng nhân viên")]
+        [Range(0, int.MaxValue, ErrorMessage = "Vui lòng nhập số")]
+        public int NumberEmployee { get; set; }
+
+        [DisplayName("Địa chỉ liên hệ")]
+        [StringLength(100)]
+        public string Address { get; set; }
+
+        [DisplayName("Địa chỉ Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DefaultValue(false)]
+        public bool Status { get; set; }
+    }
+}
