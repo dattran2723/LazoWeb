e96bffb7f21f07dd86d9166ec06bc8538fb2cb38
long đoàn <doanthanhlong1801@gmail.com>
2018-10-31 11:29:14 +0700

    add sql server project

---
+++ lazowebDb/dbo/Tables/Customers.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Customers] (
+    [ID]             INT            IDENTITY (1, 1) NOT NULL,
+    [Name]           NVARCHAR (50)  NULL,
+    [Company]        NVARCHAR (50)  NULL,
+    [NumberEmployee] INT            NULL,
+    [Address]        NVARCHAR (100) NULL,
+    [Email]          NVARCHAR (50)  NULL,
+    [Status]         BIT            NULL,
+    CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
