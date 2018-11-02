0c149ce5e9d152f23d7d8eaa9e828d581b91c92b
Tran Dat <Tran Dat@DESKTOP-SLFVQSH>
2018-11-02 16:27:01 +0700

    sf

--- LazoWeb/Models/Customer.cs
+++ LazoWeb/Models/Customer.cs
@@ -14,27 +14,27 @@ namespace LazoWeb.Models
         public int ID { get; set; }
 
         [DisplayName("Họ và tên")]
-        [Required(ErrorMessage = "Vui lòng nhập họ và tên")]
+        [Required(ErrorMessage = "Vui lòng nhập họ và tên!")]
         [StringLength(50)]
         public string Name { get; set; }
 
         [DisplayName("Tên công ty")]
-        [Required(ErrorMessage = "Vui lòng nhập tên công ty")]
+        [Required(ErrorMessage = "Vui lòng nhập tên công ty!")]
         public string Company { get; set; }
 
         [DisplayName("Số lượng nhân viên")]
-        [Required(ErrorMessage = "Vui lòng nhập số lượng nhân viên")]
-        [Range(1, int.MaxValue, ErrorMessage = "Vui lòng nhập số lớn hơn 0")]
+        [Required(ErrorMessage = "Vui lòng nhập số lượng nhân viên!")]
+        [Range(1, int.MaxValue, ErrorMessage = "Vui lòng nhập số lượng nhân viên lớn hơn 0!")]
         public int NumberEmployee { get; set; }
 
         [DisplayName("Địa chỉ liên hệ")]
-        [Required(ErrorMessage = "Vui lòng nhập địa chỉ liên hệ")]
+        [Required(ErrorMessage = "Vui lòng nhập địa chỉ liên hệ!")]
         [StringLength(100)]
         public string Address { get; set; }
 
         [DisplayName("Địa chỉ Email")]
-        [Required(ErrorMessage ="Vui lòng nhập Email")]
-        [EmailAddress]
+        [Required(ErrorMessage ="Vui lòng nhập Email!")]
+        [EmailAddress(ErrorMessage ="Địa chỉ Email không hợp lệ!")]
         public string Email { get; set; }
 
         [DefaultValue(false)]
