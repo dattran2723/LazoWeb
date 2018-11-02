0215072cbd77ac9a0c42271027ce5df41a0d42e7
long đoàn <doanthanhlong1801@gmail.com>
2018-11-02 11:48:42 +0700

    commit update manager user and ..

--- LazoWeb/Areas/Admin/Controllers/UserController.cs
+++ LazoWeb/Areas/Admin/Controllers/UserController.cs
@@ -1,5 +1,7 @@
 ﻿using LazoWeb.Models;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web.Mvc;
 
 namespace LazoWeb.Areas.Admin.Controllers
@@ -11,6 +13,95 @@ namespace LazoWeb.Areas.Admin.Controllers
         public ActionResult GetAllUser()
         {
             return View(db.Users.ToList());
+        }
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationUser model = db.Users.Find(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            return View(model);
+        }
+        
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationUser user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Admin/Customers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ApplicationUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("GetAllUser");
+            }
+            return View(user);
+        }
+
+        // GET: Admin/Customers/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationUser user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Admin/Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            ApplicationUser user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("GetAllUser");
+        }
+        [HttpPost]
+        public JsonResult ChangeStatus(string id)
+        {
+            var user = db.Users.Find(id);
+            user.EmailConfirmed = !user.EmailConfirmed;
+            db.SaveChanges();
+            var kq = user.EmailConfirmed;
+            return Json(new
+            {
+                status = kq
+            });
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
--- LazoWeb/Migrations/201811020146459_init.cs
+++ LazoWeb/Migrations/201811020146459_init.cs
@@ -3,7 +3,7 @@ namespace LazoWeb.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class initdb : DbMigration
+    public partial class init : DbMigration
     {
         public override void Up()
         {
