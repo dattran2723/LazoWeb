f5f6fe6e97ed3d8f609fc5f966dbb4ccec0e2ac0
Tran Dat <Tran Dat@DESKTOP-SLFVQSH>
2018-11-05 15:08:46 +0700

    change captcha in register.cshtml

---
+++ LazoWeb/Models/RecaptchaResult.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace LazoWeb.Models
+{
+    [DataContract]
+    public class RecaptchaResult
+    {
+        [DataMember(Name ="success")]
+        public bool Success { get; set; }
+        [DataMember(Name = "error-codes")]
+        public string[] ErrorCodes { get; set; }
+    }
+}
