e8a44b08ecac590df70a4198291a8ab8bde8312a
Tran Dat <Tran Dat@DESKTOP-SLFVQSH>
2018-11-07 09:33:06 +0700

    add lazoDb projectt

---
+++ LazoDb/dbo/Tables/Customers.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Customers] (
+    [ID]             INT            IDENTITY (1, 1) NOT NULL,
+    [Name]           NVARCHAR (50)  NOT NULL,
+    [Company]        NVARCHAR (MAX) NOT NULL,
+    [NumberEmployee] INT            NOT NULL,
+    [Address]        NVARCHAR (100) NOT NULL,
+    [Email]          NVARCHAR (MAX) NOT NULL,
+    [Status]         BIT            NOT NULL,
+    CONSTRAINT [PK_dbo.Customers] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
