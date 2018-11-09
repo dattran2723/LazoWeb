9f8f7e34807123f047af08f58c6b8f0708c3831d
Tran Dat <Tran Dat@DESKTOP-SLFVQSH>
2018-11-09 09:32:49 +0700

    commit changes

--- LazoWeb/Scripts/js/quocjs.js
+++
@@ -1,9 +0,0 @@
-﻿
-function myFunction() {
-    var x = document.getElementById("header-left");
-    if (x.className == "menu-header-left") {
-        x.className += "quocjs";
-    } else {
-        x.className = "menu-header-left";
-    }
-}
