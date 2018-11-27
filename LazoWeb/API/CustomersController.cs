81c487cd7f7438a0368ec9cae20ec0ecb69610fc
long đoàn <doanthanhlong1801@gmail.com>
2018-11-27 11:00:42 +0700

    up api

---
+++ LazoWeb/API/CustomersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LazoWeb.Models;
+using LazoWeb.Models.Repository;
+
+namespace LazoWeb.API
+{
+    public class CustomersController : ApiController
+    {
+        private ICustomerRepository customerRepository;
+
+        public CustomersController()
+        {
+            this.customerRepository = new CustomerRepository(new ApplicationDbContext());
+        }
+
+        [HttpGet]
+        [Route("api/customers")]
+        public DataTableResponse GetCustomers()
+        {
+            var customer = customerRepository.GetCustomers();
+            return new DataTableResponse
+            {
+                recordsTotal = customer.Count(),
+                recordsFiltered = customer.Count(),
+                data = customer.OrderByDescending(x => x.RegisterDate).ToArray()
+            };
+        }
+    }
+}
---
+++ LazoWeb/Models/Repository/CustomerRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazoWeb.Models.Repository
+{
+    public interface ICustomerRepository
+    {
+        IQueryable<Customer> GetCustomers();
+    }
+    public class CustomerRepository : ICustomerRepository
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public CustomerRepository(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IQueryable<Customer> GetCustomers()
+        {
+            return db.Customers;
+        }
+
+    }
+}
