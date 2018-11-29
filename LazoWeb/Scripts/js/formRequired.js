c3bbbdc6cb41f694faa7e8b2755668f309e1fa3f
Tran Dat <Tran Dat@DESKTOP-SLFVQSH>
2018-11-29 17:14:42 +0700

    changes css

--- LazoWeb/Scripts/js/formRequired.js
+++ LazoWeb/Scripts/js/formRequired.js
@@ -1,4 +1,7 @@
 ﻿$(document).ready(function () {
     $(".captcha a").html('<h4 style="float:left;"><i class="fas fa-sync-alt"></i></h4>');
-    $("#CaptchaInputText").attr("placeholder", "Nhập mã...");
+    $("#CaptchaInputText").attr({
+        placeholder: "nhập mã...",
+        style: "text-transform: uppercase"
+    });
 });
